8d5ff9e1beebdc21c83c37aaad14f803a3965c96
github username <github email address>
2019-09-23 11:24:19 -0500

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users do
+        resources :boards, shallow: true
+      end
+      resources :boards do 
+        resources :images, shallow: true
+        resources :goals, shallow: true
+      end
       resources :images
-      resources :boards
       resources :goals
     end
   end
 end
+
+
