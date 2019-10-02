55921e1f1001c22ffa752f68f6f3e5be6af85145
github username <github email address>
2019-10-02 13:43:30 -0500

    added id to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :first_name, :last_name, :email, :profile_photo, :location
+    attributes :id, :first_name, :last_name, :email, :profile_photo, :location
   end
